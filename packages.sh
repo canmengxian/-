5ee3fe9f542c22349f96a99858feac926ee61a08
残梦 <37858783+canmengxian@users.noreply.github.com>
2021-04-06 20:05:57 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,25 +1,28 @@
 git clone --depth 1 https://github.com/gdck/luci-app-control-weburl.git
 git clone --depth 1 https://github.com/sensec/luci-app-xlnetacc.git
 git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset.git
-git clone --depth 1 https://github.com/BoringCat/luci-app-mentohust
-git clone --depth 1 https://github.com/project-lede/luci-app-godproxy
-git clone --depth 1 https://github.com/tty228/luci-app-serverchan
-git clone --depth 1 https://github.com/BoringCat/luci-app-minieap
-git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr
-git clone --depth 1 https://github.com/ysc3839/luci-proto-minieap
-git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced
-git clone --depth 1 https://github.com/jerrykuku/luci-app-jd-dailybonus
-git clone --depth 1 https://github.com/rufengsuixing/luci-app-syncdial      
-git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata
-git clone --depth 1 https://github.com/sensec/ddns-scripts_aliyun
-git clone --depth 1 https://github.com/KumaTea/MentoHUST-SYSU-Guide
-git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome
-git clone --depth 1 https://github.com/BCYDTZ/luci-app-UUGameAcc
-git clone --depth 1 https://github.com/garypang13/luci-app-eqos
+git clone --depth 1 https://github.com/BoringCat/luci-app-mentohust.git
+git clone --depth 1 https://github.com/project-lede/luci-app-godproxy.git
+git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git
+git clone --depth 1 https://github.com/BoringCat/luci-app-minieap.git
+git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr.git
+git clone --depth 1 https://github.com/ysc3839/luci-proto-minieap.git
+git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced.git
+git clone --depth 1 https://github.com/jerrykuku/luci-app-jd-dailybonus.git
+git clone --depth 1 https://github.com/rufengsuixing/luci-app-syncdial   .git   
+git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata.git
+git clone --depth 1 https://github.com/sensec/ddns-scripts_aliyun.git
+git clone --depth 1 https://github.com/KumaTea/MentoHUST-SYSU-Guide.git
+git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome.git
+git clone --depth 1 https://github.com/BCYDTZ/luci-app-UUGameAcc.git
+git clone --depth 1 https://github.com/garypang13/luci-app-eqos.git
 git clone --depth 1 https://github.com/riverscn/openwrt-iptvhelper.git 
+
 svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
 svn co https://github.com/brvphoenix/wrtbwmon/trunk/wrtbwmon
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-airwhu
+
 rm -rf ./*/.git
 rm -rf ./*/.svn 
 rm -f .gitattributes .gitignore
+exit 0
